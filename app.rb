ca1f0967756be7121ab7c272fa796e34122ff273
elliewilson1 <ewilson0@chicagobooth.edu>
2025-04-17 14:55:46 +0000

    started

--- app.rb
+++ app.rb
@@ -1,9 +1,13 @@
 require "sinatra"
 require "sinatra/reloader"
+require "dotenv/load"
 
 get("/") do
   "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
+  <h1>Currency pairs</h1>
   "
 end
+
+
+
+pp ENV.fetch("EXCHANGE_RATE_KEY")
