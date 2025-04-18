81c6157646d962b594f87c998d7b8fa88f159dfb
elliewilson1 <ewilson0@chicagobooth.edu>
2025-04-18 01:40:38 +0000

    started

--- app.rb
+++ app.rb
@@ -1,13 +1,13 @@
 require "sinatra"
 require "sinatra/reloader"
 require "dotenv/load"
+require "json"
+
+exchange_rates_api_key = ENV.fetch("EXCHANGE_RATE_KEY")
+exchange_rates_url = "https://api.exchangerate.host/list?access_key=" + exchange_rates_api_key
+raw_response = HTTP.get(exchange_rates_url)
+parsed_response = JSON.parse(raw_response)
 
 get("/") do
-  "
-  <h1>Currency pairs</h1>
-  "
+  erb(:first_currency)
 end
-
-
-
-pp ENV.fetch("EXCHANGE_RATE_KEY")
